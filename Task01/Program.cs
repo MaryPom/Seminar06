1c9d9266aa8ace17633161a99c8dfa9d33ed308a
Mariia <mashulkaegor@mail.ru>
2022-08-25 22:31:52 +0400

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1. Показать двумерный массив размером m×n заполненный вещественными числами
+
+void FillArrayRandomNumbers(double[,] mas)
+{
+ for (int m = 0; m < mas.GetLength(0); m++)
+    {
+ for (int n = 0; n < mas.GetLength(1); n++)
+        {
+ mas[m, n] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] mas)
+{
+ for (int m = 0; m < mas.GetLength(0); m++)
+    {
+        for (int n = 0; n < mas.GetLength(1); n++)
+        {
+            Console.Write(mas[m, n] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+double [,] mas = new double[m, n];
+FillArrayRandomNumbers(mas);
+PrintArray(mas);
